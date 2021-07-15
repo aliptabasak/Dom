526bd7acf130967c867fa660b59e49c1380f156b
Alipta Basak <aliptabasaky2k@gmail.com>
2021-07-15 10:58:04 +0530

    adding html and js file

---
+++ dom.js
@@ -0,0 +1,19 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var title=document.getElementsByClassName('title');
+// console.log(title[0]);
+title[0].style.fontWeight='bold';
+title[0].style.color='green';
